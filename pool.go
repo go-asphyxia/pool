f2ad12255c0ce0480dfa50c4bcd4beed7ec5dd7d
cmetallo <denerohin@yandex.ru>
2022-03-09 23:02:47 +0300

    initial

--- pool.go
+++ pool.go
@@ -1,5 +1,7 @@
 package pool
 
+import "sync/atomic"
+
 type (
 	Constructor func() (itf interface{})
 
@@ -8,30 +10,54 @@ type (
 	Pool struct {
 		Constructor Constructor
 		Destructor  Destructor
-		Current     int
+		Current     uint32
 		Storage     chan interface{}
+		Limit uint32
+		close uint32
 	}
 )
 
-func NewPool(constructor Constructor, destructor Destructor, limit int) (p *Pool) {
+func NewPool(constructor Constructor, destructor Destructor, limit uint32) (p *Pool) {
 	p = &Pool{
 		Constructor: constructor,
 		Destructor:  destructor,
 		Storage:     make(chan interface{}, limit),
+		Limit: limit,
 	}
 
 	return
 }
 
 func (p *Pool) Get() (itf interface{}) {
+	if p.close == 1 {
+		return
+	}
+	if p.Current == p.Limit {
+		select {
+		case itf = <-p.Storage:
+		}
+		return
+	}
 	select {
 	case itf = <-p.Storage:
 	default:
 		itf = p.Constructor()
+		atomic.AddUint32(&p.Current, 1)
 	}
 
 	return
 }
 func (p *Pool) Put(itf interface{}) {
 	p.Storage <- itf
+}
+
+func (p *Pool) Close() (err error) {
+	if !atomic.CompareAndSwapUint32(&p.close, 0, 1) {
+		return
+	}
+	for i := 0; i < int(p.Current); i++ {
+		itf := <- p.Storage
+		p.Destructor(itf)
+	}
+	return
 }
---
+++ pool_test.go
@@ -0,0 +1,24 @@
+package pool
+
+import (
+	"sync"
+	"testing"
+)
+
+func BenchmarkOurPool(b *testing.B) {
+	p := NewPool(func() (itf interface{}) { itf = 1488; return }, func(itf interface{}) {}, 100)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		o := p.Get()
+		p.Put(o)
+	}
+}
+
+func BenchmarkStandartPool(b *testing.B) {
+	p := sync.Pool{ New: func() (itf interface{}) {itf = 1488; return}}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		o := p.Get()
+		p.Put(o)
+	}
+}
