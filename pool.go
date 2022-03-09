3c71944c6e8b2e049b42480464cb194c2f2a7264
cmetallo <denerohin@yandex.ru>
2022-03-09 20:28:10 +0300

    initial2

--- pool.go
+++ pool.go
@@ -1,2 +1,37 @@
 package pool
 
+type (
+	Constructor func() (itf interface{})
+
+	Destructor func(itf interface{})
+
+	Pool struct {
+		Constructor Constructor
+		Destructor  Destructor
+		Current     int
+		Storage     chan interface{}
+	}
+)
+
+func NewPool(constructor Constructor, destructor Destructor, limit int) (p *Pool) {
+	p = &Pool{
+		Constructor: constructor,
+		Destructor:  destructor,
+		Storage:     make(chan interface{}, limit),
+	}
+
+	return
+}
+
+func (p *Pool) Get() (itf interface{}) {
+	select {
+	case itf = <-p.Storage:
+	default:
+		itf = p.Constructor()
+	}
+
+	return
+}
+func (p *Pool) Put(itf interface{}) {
+	p.Storage <- itf
+}
